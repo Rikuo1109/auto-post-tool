a2661ef049e891ad06ea9ab2c0dd6929483254db
chanhtran2410 <tranletrungchanh12345@gmail.com>
2023-06-26 09:15:46 +0700

    update: reset link

--- auto_post_tool/router/authenticate.py
+++ auto_post_tool/router/authenticate.py
@@ -18,20 +18,20 @@ class AuthBearer(HttpBearer):
             login_token = LoginToken.objects.get(token=token)
         except LoginToken.DoesNotExist:
             raise NotFound(message_code="LOGIN_TOKEN_NOT_FOUND")
-
+        """Check login token status"""
         if not login_token.active:
             raise AuthenticationFailed(message_code="INVALID_LOGIN_TOKEN")
         try:
             access_token = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.JWT_ALGORITHM)
         except jwt.exceptions.DecodeError as exc:
             raise AuthenticationFailed(message_code="INVALID_LOGIN_TOKEN")
-
-        user_uid = access_token.get("user_uid")
-        exp_time = access_token.get("exp")
-
-        if user_uid is None or exp_time is None:
+        """Parsing JWT"""
+        try:
+            user_uid = access_token.get("user_uid")
+            exp_time = access_token.get("exp")
+        except user_uid is None or exp_time is None:
             raise ParseError(message_code="INVALID_LOGIN_TOKEN")
-
+        """Check exp of JWT"""
         if datetime.fromtimestamp(exp_time) < datetime.now():
             raise AuthenticationFailed(message_code="INVALID_LOGIN_TOKEN")
 
@@ -39,6 +39,6 @@ class AuthBearer(HttpBearer):
             request.user = User.objects.get(uid=user_uid)
             return token
         except jwt.exceptions.DecodeError:
-            return {"detail": "Invalid access token."}
+            raise AuthenticationFailed(message_code="INVALID_LOGIN_TOKEN")
         except User.DoesNotExist:
-            return {"detail": "User does not exist."}
+            raise NotFound(message_code="USER_NOT_FOUND")
--- auto_post_tool/user_account/services/create_reset_link.py
+++ auto_post_tool/user_account/services/create_reset_link.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 class CreateResetLinkService:
     def create_reset_link(self, token):
-        return f"{settings.FRONTEND_HOST_URL}/reset-password/{token}"
+        return f"{settings.FRONTEND_HOST_URL}/#/reset-password/{token}"
