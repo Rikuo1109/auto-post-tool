9f94c5a44a515f701365ec7c3619954026d20ba8
Hieu Nguyen <hieu.nguyen188@hcmut.edu.vn>
2023-06-26 15:35:24 +0700

    refactor: shorten request creating func

--- auto_post_tool/utils/services/Zalo/__init__.py
+++ auto_post_tool/utils/services/Zalo/__init__.py
@@ -8,7 +8,7 @@ class ZaloService(object):
         self.app_secret = settings.ZALO_API_APP_SECRET
         self.request_content_type = settings.ZALO_API_REQUEST_CONTENT_TYPE
 
-    def request_access_oath(self, oath_code: str):
+    def get_response(self, data):
         return requests.request(
             "POST",
             settings.ZALO_ACCESS_TOKEN_URL,
@@ -16,16 +16,11 @@ class ZaloService(object):
                 "secret_key": settings.ZALO_API_APP_SECRET,
                 "Content-Type": settings.ZALO_API_REQUEST_CONTENT_TYPE,
             },
-            data=f"app_id={self.app_id}&grant_type=authorization_code&code={oath_code}",
+            data=data,
         )
 
+    def request_access_oath(self, oath_code: str):
+        return self.get_response(data=f"app_id={self.app_id}&grant_type=authorization_code&code={oath_code}")
+
     def request_access_fefresh(self, refresh_token: str):
-        return requests.request(
-            "POST",
-            settings.ZALO_ACCESS_TOKEN_URL,
-            headers={
-                "secret_key": settings.ZALO_API_APP_SECRET,
-                "Content-Type": settings.ZALO_API_REQUEST_CONTENT_TYPE,
-            },
-            data=f"app_id={self.app_id}&grant_type=refresh_token&code={refresh_token}",
-        )
+        return self.get_response(data=f"app_id={self.app_id}&grant_type=refresh_token&code={refresh_token}")
