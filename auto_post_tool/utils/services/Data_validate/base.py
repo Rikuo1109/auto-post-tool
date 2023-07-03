5771bee7fc8863840c7b6b1d7c23175841b7031a
chanhtran2410 <tranletrungchanh12345@gmail.com>
2023-07-03 13:16:56 +0700

    Update: update data validation (must not contain space in some fields)

--- auto_post_tool/utils/services/Data_validate/base.py
+++ auto_post_tool/utils/services/Data_validate/base.py
@@ -2,11 +2,12 @@ import re
 
 CONTAIN_NUMBER = "^(?=.*\d).+$"
 EMAIL = "^[\w+\.-]+@[\w+\.-]+\.\w+$"
+NOT_CONTAIN_SPACE = "^(?=.*\s).+$"
 
 
 class BaseValidator(object):
     @staticmethod
-    def is_longger_than(value: str, max_length: int) -> bool:
+    def is_longer_than(value: str, max_length: int) -> bool:
         return bool(len(value) > max_length)
 
     @staticmethod
@@ -16,3 +17,7 @@ class BaseValidator(object):
     @staticmethod
     def check_email_format(value: str):
         return bool(re.match(EMAIL, value))
+
+    @staticmethod
+    def is_contain_space(value: str):
+        return bool(re.match(NOT_CONTAIN_SPACE, value))
